fb07f01d2ff511c077aa8fe4bc1e0d3e051e6f60
Suyash Bhawsar <suyashbhawsar@hotmail.com>
2022-12-16 10:44:25 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,22 @@ import { getFileBySlug } from '@/lib/mdx'
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
 export async function getStaticProps() {
-  const authorDetails = await getFileBySlug('authors', ['default', 'suyash'])
+  const authorDetails = await getFileBySlug('authors', ['default'])
+  return { props: { authorDetails } }
+}
+
+export default function About({ authorDetails }) {
+  const { mdxSource, frontMatter } = authorDetails
+
+  return (
+    <MDXLayoutRenderer
+      layout={frontMatter.layout || DEFAULT_LAYOUT}
+      mdxSource={mdxSource}
+      frontMatter={frontMatter}
+    />
+  )
+export async function getStaticProps() {
+  const authorDetails = await getFileBySlug('authors', ['suyash'])
   return { props: { authorDetails } }
 }
 
