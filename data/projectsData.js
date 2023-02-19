0b66db2b5f16ac03909c2fadc2b194ff7becf11e
Suyash Bhawsar <suyashbhawsar@hotmail.com>
2023-02-20 01:36:33 +0530

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,8 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `Try DuckDuckGo.`,
-    href: 'https://www.duckduckgo.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '-',
+    description: `-`,
+    href: '-',
   },
 ]
 
